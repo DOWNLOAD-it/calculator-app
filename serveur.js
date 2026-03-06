3f39fda12398a5551a1c16d3dced3b3d0786137c
abderrahmane <dodsoloman@outlook.fr>
2026-03-06 22:53:44 +0000

    Test webhook

---
+++ serveur.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/add/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  res.send({ result: a + b });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+console.log("hello 123");
+
+module.exports = app;
